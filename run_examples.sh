05a4784addd3746a926eeeb4cbe4456576b98d8f
Sudhir Reddy <sudhir.reddy@flipkart.com>
2019-07-04 10:01:41 +0530

    Fixing Memory Limits needed for Example 2

--- run_examples.sh
+++ run_examples.sh
@@ -1,8 +1,14 @@
+#!/usr/bin/env bash
 #/bin/bash
 if [ $# -eq 0 ]; then
 	echo "Wrong Usage ! Pass Example to Run as arguments."
 	exit 1
 fi
 export QUEUE=mlp
+if [$1 -eq 2 ]; then
+	export MAX_MEM_EXECUTOR=14G
+else
+	export MAX_MEM_EXECUTOR=1024m
+fi
 export JMX="-Dcom.sun.management.jmxremote.port=0 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
-spark-submit --master yarn --conf spark.executor.extraJavaOptions="$JMX -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/ -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" --class org.flipkart.fdp.debugging_problems.ExampleLauncher --deploy-mode cluster --conf spark.task.maxFailures=1 --conf spark.executor.memory=1024m --conf spark.yarn.maxAppAttempts=1 --queue $QUEUE debugging_problems-1.0-SNAPSHOT.jar $1
+spark-submit --master yarn --conf spark.executor.extraJavaOptions="$JMX -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/ -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" --class org.flipkart.fdp.debugging_problems.ExampleLauncher --deploy-mode cluster --conf spark.task.maxFailures=1 --conf spark.executor.memory=$MAX_MEM_EXECUTOR --conf spark.yarn.maxAppAttempts=1 --queue $QUEUE debugging_problems-1.0-SNAPSHOT.jar $1
